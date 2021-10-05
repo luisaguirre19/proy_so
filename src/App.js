a0d6831965adebe67872193d86c604ea2963c9f4
Luis Aguirre <luisaguirre@MacBook-Pro-de-Luis.local>
2021-10-04 21:52:11 -0600

    hola mundo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+          <h1 className="App-title">HOLA MUNDO xs</h1>
         </header>
         <p className="App-intro">
           Welcome to this Docker Meetup with Jenkins
