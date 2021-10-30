7cd0ae3be1ef3039299bf1c4c61fe517e956b08a
selvin95 <s.eliezer.perez@gmail.com>
2021-10-30 12:12:20 -0600

    Se modifico Sistemas Operativos 1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">Proyecto final sistemas operativos 2.</h1>
-            <h3>La finalidad de este proyecto es demostrar los conocimientos adquiridos en el curso de sistemas operativos 2</h3>
+            <h3>La finalidad de este proyecto es demostrar los conocimientos adquiridos en el curso de sistemas operativos 1</h3>
             <hr />
             <ul className="social">
                {networks}
